e3868d46a9d0dd640a3c7ba23d019aa192799d8d
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-02-27 01:42:53 +0900

    2003번 풀이(많은 것을 배워갑니다..)

--- by_JavaScript/Silver/2003(풀이중)_S4.js
+++ by_JavaScript/Silver/2003(풀이중)_S4.js
@@ -10,37 +10,44 @@ let [N, M] = input[0].split(' ').map(Number);
 
 /*
   수열의 i번째 수부터 j번째 수까지의 합 A[i] + .. + A[j]가 M이 되는 경우의 수?
-  => 부분집합을 탐색하면서, 그것이 M이 되는지 아닌지 확인하면 그만 아님?
+  => 시간 제한 0.5초 / Sequential Search로 일일이 다 찾는 건.. 분명 시간 터짐
+  => N의 개수가 10,000개, 부분집합을 일일이 탐색하려면..? 2^10000개 다 탐색? 말도 안되는 짓이지!
+  => 연속된 원소의 집합에서 찾는거임..
+    -> 투 포인터 쓰면 되는 거 아님?
 */
 
 let num_list = [];
 
 //우선 수열 입력 받기
-for(let i = 0; i < N; i++) {
-  num_list = input[1].split(' ').map(Number);
-}
+num_list = input[1].split(' ').map(Number);
 
 let sum = 0;
 let answer = 0;
+let start = 0, finish = 0;
 
-//부분집합은 재귀로 호출한다
-function subset(depth, len) {
-  //기저조건 도달(subset 함수 N번 불렀을 경우..)
-  if(depth === N) {
-    if(sum === M) answer++; //부분 집합의 합이 M이 되는 경우에만 경우의 수(answer)++
-    return;
+/* 
+  연속된 수의 집합에서 결과를 도출해야 함.. --> 투 포인터!
+  sum에 더하기 빼기 하는 연산은.. 수열의 모든 원소에 대해 한 번씩만 진행 --> 최대 10,000번
+*/
+while (finish < N) {
+  if (sum < M) {
+    sum += num_list[finish];
+    finish++;
+  } else {
+    if (sum === M) answer++;
+    sum -= num_list[start];
+    start++;
   }
-
-  //특정 원소를 선택하지 않음
-  subset(depth+1, len);
-
-  //특정 원소를 선택함 (선택해서 더했다고 가정) --> 선택 했으니 부분집합에서 선택할 인덱스(len)+1
-  //하
-  sum += num_list[len];
-  subset(depth+1, len+1);
-  sum -= num_list[len];
 }
 
-//subset 함수 호출 후, 결과 출력
-subset(0, 0);
+/* 
+  마지막 부분합이 M인지 추가 검사 
+  --> finish가 N이 되었어도.. start를 증가 시키면서 남아있는 답을 찾아내야 함
+*/
+while (sum >= M) {
+  if (sum === M) answer++;
+  sum -= num_list[start];
+  start++;
+}
+
 console.log(answer);
